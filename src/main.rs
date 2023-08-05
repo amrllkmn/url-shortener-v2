57d7a17f7ffd86f023d3b8a5829bd8b062fc6e0f
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-08-05 23:00:53 +0800

    Dockerize application

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> std::io::Result<()> {
         .configure(api_v1::api_service)
         .service(healthcheck)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
