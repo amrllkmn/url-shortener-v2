2fc1b72bd60cbc9d793f9b1d72d73ff0c68d93f1
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-08-24 18:51:16 +0800

    Run migration before starting app

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
+use migration::{Migrator, MigratorTrait};
 use sea_orm::{Database, DatabaseConnection};
 
 mod entities;
@@ -18,6 +19,9 @@ async fn healthcheck() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     let db_url = String::from("postgres://postgres:postgres@localhost:5434/url_shortener_dev"); // Hardcoding it cos for now it's a docker db server
     let db_conn = Database::connect(&db_url).await.unwrap();
+
+    // Run migrations before connecting to database
+    Migrator::up(&db_conn, None).await.unwrap();
     let state = AppState { conn: db_conn };
 
     HttpServer::new(move || {
@@ -25,6 +29,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(state.clone()))
             .configure(urls::service)
             .service(healthcheck)
+            .wrap(middleware::Logger::default())
     })
     .bind(("127.0.0.1", 8080))?
     .run()
