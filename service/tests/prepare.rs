afb101ce74640c121f63e0a5e79bf3ac7a24678f
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2023-08-31 22:04:51 +0800

    Add more tests

--- service/tests/prepare.rs
+++ service/tests/prepare.rs
@@ -5,12 +5,14 @@ use sea_orm::*;
 
 #[cfg(feature = "mock")]
 pub fn prepare_mock_db() -> DatabaseConnection {
-    use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
+    use chrono::{NaiveDate, NaiveDateTime, NaiveTime, Utc};
     // Create a NaiveDate
     let date = NaiveDate::from_ymd_opt(2023, 8, 31).expect("Invalid date components");
 
     // Create a NaiveTime
     let time = NaiveTime::from_hms_opt(15, 30, 0).expect("Invalid time components");
+
+    let newtime = Utc::now().naive_utc();
 
     // Combine the NaiveDate and NaiveTime to create a NaiveDateTime
     let datetime = NaiveDateTime::new(date, time);
@@ -41,14 +43,32 @@ pub fn prepare_mock_db() -> DatabaseConnection {
                 updated_at: datetime,
             },
         ]])
+        .append_query_results([
+            [url::Model {
+                id: 3,
+                title: "Google".to_string(),
+                url: "https://google.com".to_string(),
+                slug: "testing".to_string(),
+                created_at: newtime,
+                updated_at: newtime,
+            }],
+            [url::Model {
+                id: 4,
+                title: "Google".to_string(),
+                url: "https://google.com".to_string(),
+                slug: "testing".to_string(),
+                created_at: newtime,
+                updated_at: newtime,
+            }],
+        ])
         .append_exec_results([
             MockExecResult {
-                last_insert_id: 14,
-                rows_affected: 4,
+                last_insert_id: 3,
+                rows_affected: 1,
             },
             MockExecResult {
-                last_insert_id: 6,
-                rows_affected: 5,
+                last_insert_id: 4,
+                rows_affected: 1,
             },
         ])
         .into_connection()
--- service/tests/test.rs
+++ service/tests/test.rs
@@ -1,5 +1,6 @@
 mod prepare;
 
+use chrono::Utc;
 use entities::url;
 use prepare::*;
 use sea_orm::DatabaseConnection;
@@ -9,6 +10,7 @@ use service::{Mutation, Query};
 async fn main() {
     let db: &DatabaseConnection = &prepare_mock_db();
 
+    // Find by slug
     {
         let url = Query::find_by_slug(db, "testing".to_string())
             .await
@@ -18,8 +20,34 @@ async fn main() {
         assert_eq!(url.id, 1);
     }
 
+    // Find all
     {
         let urls = Query::find_all(db).await.unwrap();
         assert_eq!(urls.len(), 2);
+    }
+
+    // Create a URL
+    {
+        let time = Utc::now().naive_utc();
+        let url = Mutation::create(
+            db,
+            url::Model {
+                id: 3,
+                title: "Google".to_string(),
+                url: "https://google.com".to_string(),
+                slug: "testing".to_string(),
+                created_at: time,
+                updated_at: time,
+            },
+        )
+        .await
+        .unwrap();
+        assert_eq!(url.id, 3);
+    }
+
+    // Delete URL
+    {
+        let result = Mutation::delete(db, 4).await.unwrap();
+        assert_eq!(result.rows_affected, 1);
     }
 }
